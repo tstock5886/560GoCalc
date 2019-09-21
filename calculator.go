718729e871c557eaaebad365b025d8fe0d283d6d
Travis Stockwell <travis.stockwell@gmail.com>
2019-09-21 11:23:50 -0600

    First commit.

---
+++ calculator.go
@@ -0,0 +1,75 @@
+package calculator
+
+import (
+	"errors"
+	"math"
+	"strconv"
+)
+
+// MyNum is our internal representation of a number
+type MyNum struct {
+	n float64
+}
+
+// GetNumFromOct converts string Oct to MyNum
+func GetNumFromOct(s string) *MyNum {
+	retval := new(MyNum)
+	anInt64, _ := strconv.ParseInt(s, 8, 64)
+	retval.n = float64(anInt64)
+	return retval
+}
+
+// GetNumFromHex converts string Hex to MyNum
+func GetNumFromHex(s string) *MyNum {
+	retval := new(MyNum)
+	anInt64, _ := strconv.ParseInt(s, 16, 64)
+	retval.n = float64(anInt64)
+	return retval
+}
+
+// GetNumFromInt converts int to MyNum
+func GetNumFromInt(i int64) *MyNum {
+	retval := new(MyNum)
+	retval.n = float64(i)
+	return retval
+}
+
+// Multiply two MyNums together
+func (m *MyNum) Multiply(n *MyNum) *MyNum {
+	m.n *= n.n
+	return m
+}
+
+// Add two MyNums together
+func (m *MyNum) Add(n *MyNum) *MyNum {
+	m.n += n.n
+	return m
+}
+
+// Subtract a MyNum
+func (m *MyNum) Subtract(n *MyNum) *MyNum {
+	m.n -= n.n
+	return m
+}
+
+// Divide a MyNum
+func (m *MyNum) Divide(n *MyNum) (*MyNum, error) {
+	// What if 'n' is zero??
+	if n.n == 0 {
+		return m, errors.New("attempt to divide by zero")
+	}
+	m.n /= n.n
+	return m, nil
+}
+
+// SqRoot will get square root of this number
+func (m *MyNum) SqRoot() *MyNum {
+	m.n = math.Sqrt(m.n)
+	return m
+}
+
+// Exponent this number
+func (m *MyNum) Exponent(n *MyNum) *MyNum {
+	m.n = math.Pow(m.n, n.n)
+	return m
+}
---
+++ calculator_test.go
@@ -0,0 +1,53 @@
+package calculator
+
+import (
+	"testing"
+)
+
+func TestConv(t *testing.T) {
+	a := GetNumFromHex("0A")
+	if a.n != 10 {
+		t.Errorf("Hex 0A should be 10, but we have %f\n", a.n)
+	}
+	b := GetNumFromOct("12")
+	if b.n != 10 {
+		t.Errorf("Oct 11 should be 10, but we have %f\n", b.n)
+	}
+	c := GetNumFromInt(100)
+	if c.n != 100 {
+		t.Errorf("Int 100 should be 100, but we have %f\n", c.n)
+	}
+	a.Add(b)
+	if a.n != 20 {
+		t.Errorf("Add 10 to 10 should be 20, but we have %f\n", a.n)
+	}
+	a.Subtract(b)
+	if a.n != 10 {
+		t.Errorf("Sub 10 from 20 should be 10, but we have %f\n", a.n)
+	}
+	a.Multiply(b)
+	if a.n != 100 {
+		t.Errorf("Multiply 10 and 10 should be 100, but we have %f\n", a.n)
+	}
+	a.Divide(b)
+	if a.n != 10 {
+		t.Errorf("Divide 100 by 10 should be 10, but we have %f\n", a.n)
+	}
+	zero := GetNumFromInt(0)
+	_, err := a.Divide(zero)
+	if err == nil {
+		t.Errorf("Divide by zero should throw an error\n")
+	}
+	five := GetNumFromInt(5)
+	a.Exponent(five)
+	if a.n != 100000 {
+		t.Errorf("The 10^5 should be 100000. We got %f\n", a.n)
+	}
+
+	oneHundred := GetNumFromInt(100)
+	oneHundred.SqRoot()
+	if oneHundred.n != 10 {
+		t.Errorf("The sqroot of 100 should be 10. We got %f\n", oneHundred.n)
+	}
+
+}
